ad956fb297d7b0b00b8bf685f8eadd3b2591111e
jimrothstein <jimrothstein@gmail.com>
2024-01-22 22:08:28 -0800

    wip

--- ZSH_SH_FILES/082_save_regex_to_file.sh
+++ ZSH_SH_FILES/082_save_regex_to_file.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh 
+#   082_save_regex_to_file.sh
 #
-# 081_FD_save_to_log.sh
 #	REF"
 # https://blog.tratif.com/2023/01/09/bash-tips-1-logging-in-shell-scripts/?utm_source=pocket_saves
 #
@@ -19,7 +19,7 @@ log() {
     echo "[$(date -I"minutes")]" "$@"
 }
 
-LOGFILE="./log.log"
+LOGFILE="./regex.log"
 set -x  # print line of code, before evaluated 
 exec 3>&1 1>"$LOGFILE" 2>&1
 
@@ -30,6 +30,8 @@ echo "1"
 echo "2"
 echo "3"
 echo "hi1" >&3   # console, only; not log.log
+
+echo "abc" | grep -P "a"
 
 log "without redirect" 
 log "with redirect"	>&3
